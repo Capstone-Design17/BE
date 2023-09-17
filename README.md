9e44c8e755fd3c110dd4da51ad56c72df6df45ed
Hyunuk <dksgusdnr17@naver.com>
2023-09-17 20:27:02 +0900

    [tomato-market/plan#2] .editorconfig 추가
    .editorconfig, intellij 내부 setting 옵션을 이용하여 checkstyle 적용
    성공

--- README.md
+++ README.md
@@ -3,20 +3,14 @@
 Tomato Market backend
 
 ## Version
-- Spring Boot 3.1.3
 
+- Spring Boot 3.1.3
 
 ## Installation
 
-
-
 ## Running the app
 
-
-
 ## Build
-
-
 
 ## Test
 
--- src/test/java/com/tomato/market/controller/HelloControllerTest.java
+++ src/test/java/com/tomato/market/controller/HelloControllerTest.java
@@ -1,23 +1,23 @@
 package com.tomato.market.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @WebMvcTest(HelloController.class)
 public class HelloControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Test
-    void getHelloTest() throws Exception {
-        mockMvc.perform(get("/hello")).andExpect(status().isOk()).andDo(print());
-    }
+	@Test
+	void getHelloTest() throws Exception {
+		mockMvc.perform(get("/hello")).andExpect(status().isOk()).andDo(print());
+	}
 
 }
