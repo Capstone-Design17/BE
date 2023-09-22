8bb3995ef63aa871cf9c47f9bdb44e8f09a3b58e
Hyunuk <dksgusdnr17@naver.com>
2023-09-23 04:35:25 +0900

    [tomato-market/plan#2] Logback 설정

--- src/main/java/com/tomato/market/controller/HelloController.java
+++ src/main/java/com/tomato/market/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.tomato.market.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
 	@GetMapping("/hello")
 	public String getHello() {
+		logger.info("HelloController is called");
+		logger.warn("HelloController is called");
+
 		return "Hello  World!";
 	}
 }
