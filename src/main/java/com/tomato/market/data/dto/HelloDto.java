e0803d04d41df2d0a6b2855daf8b445e42502e59
Hyunuk <dksgusdnr17@naver.com>
2023-09-23 11:33:07 +0900

    [tomato-market/plan#2] 기본 패키지 구성 및 DB 연결
    프로파일 분리 local, dev
    Controller, Service, DAO, Repository 구조
    DTO, Entity를 통한 데이터 전달
    JDBC 연결 및 JPA 설정

---
+++ src/main/java/com/tomato/market/data/dto/HelloDto.java
@@ -0,0 +1,15 @@
+package com.tomato.market.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HelloDto {
+	String id;
+	String text;
+}
