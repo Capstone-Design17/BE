0e2e501c9db3cc0b26a7b81fd4743b41422a0d6c
Hyunuk <dksgusdnr17@naver.com>
2023-10-07 14:30:29 +0900

    [tomato-market/plan#17] 브랜치 생성

--- src/main/java/com/tomato/market/controller/UserController.java
+++ src/main/java/com/tomato/market/controller/UserController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,5 +66,9 @@ public class UserController {
 
 		// 로그인 성공 or 실패 redirect
 		return "로그인 성공"; // responseDto 반환할 것
+	}
+
+	@DeleteMapping("user/logout")
+	public void logout() {
 	}
 }
