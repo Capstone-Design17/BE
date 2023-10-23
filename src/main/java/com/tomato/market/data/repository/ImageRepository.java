ca7ec951202925d8770cdfaca111137798acc7c7
Hyunuk <dksgusdnr17@naver.com>
2023-10-23 20:13:04 +0900

    [release/0.2.0] Release-0.2.0 생성

--- src/main/java/com/tomato/market/data/repository/ImageRepository.java
+++ src/main/java/com/tomato/market/data/repository/ImageRepository.java
@@ -8,9 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.tomato.market.data.entity.ImageEntity;
 
 public interface ImageRepository extends JpaRepository<ImageEntity, Integer> {
-	//	Optional<ImageEntity> findImageByPostNum(Integer postNum);
 	Optional<ImageEntity> findTopByPostNumOrderByImageNum(Integer postNum);
-
 
 	List<ImageEntity> findByPostNum(Integer postNum);
 }
